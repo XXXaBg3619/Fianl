9b13e859296fb8c8038cf0d202edf2b187a8460f
XXXaBg3619 <linbaorei@gmail.com>
2023-01-05 17:52:53 +0800

    1

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,28 @@ import searchRoute from './routes/search';
 const app = express();
 
 // init middleware
-app.use(cors());
+// app.use(cors());
 
 // define routes
-app.use("/api/search", searchRoute);
+// app.use("/api/search", searchRoute);
+
+
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(cors());
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.get("/*", function (req, res) {
+      res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+      console.log(req.body);
+    });
+    app.use(express.json())
+    app.use(bodyParser.json());
+    app.use("/", routes);
+}
+if (process.env.NODE_ENV === "development") {
+	app.use(cors());
+}
+
 
 // define server
 const port = process.env.PORT || 4000;
